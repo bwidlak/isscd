3f226342d9513366aba9cdb0c6ce4173e8a5033c
Bartek <widlak@fidor.de>
2014-08-16 21:57:47 +0200

    Update font assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
