6d078bd02cbc85bb5f9f5c54bf5b7774a1404e5f
Bartek <widlak@fidor.de>
2014-09-14 19:16:31 +0200

    Change to boolean in assets attribute

--- db/migrate/20140721195606_create_assets.rb
+++ db/migrate/20140721195606_create_assets.rb
@@ -4,7 +4,7 @@ class CreateAssets < ActiveRecord::Migration
       t.integer   :project_id
       t.integer   :asset_id
       t.string    :asset_type
-      t.integer   :main, default: false
+      t.boolean   :main, default: false
 
       t.timestamps
     end
